14868cc181ea4660207393afe583a08c88de3323
Wallas9_3 <drame.sissako001@gmail.com>
2022-02-27 17:01:52 +0100

    filter logic added without ngrx

--- src/app/pages/suggestions/components/suggestions-filter/suggestions-filter.component.ts
+++ src/app/pages/suggestions/components/suggestions-filter/suggestions-filter.component.ts
@@ -1,4 +1,12 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, OnInit } from '@angular/core';
+
+export enum Filter {
+  MOST_UPVOTES = "Most Upvotes",
+  LEAST_UPVOTES = "Least Upvotes",
+  MOST_COMMENTS = "Most Comments",
+  LEAST_COMMENTS = "Least Comments"
+}
 
 @Component({
   selector: 'app-suggestions-filter',
@@ -7,9 +15,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuggestionsFilterComponent implements OnInit {
 
+  showFilter = false;
+  currentFilter = Filter.MOST_UPVOTES
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onShowFilter() {
+    this.showFilter = !this.showFilter;
+  }
+
+  onSelectFilter(filter: string) {
+    switch (filter) {
+      case Filter.MOST_COMMENTS:
+        this.currentFilter = Filter.MOST_COMMENTS;
+        break;
+      case Filter.LEAST_COMMENTS:
+        this.currentFilter = Filter.LEAST_COMMENTS;
+        break;
+      case Filter.MOST_UPVOTES:
+        this.currentFilter = Filter.MOST_UPVOTES;
+        break;
+      case Filter.LEAST_UPVOTES:
+        this.currentFilter = Filter.LEAST_UPVOTES;
+        break;
+      default:
+        this.currentFilter = Filter.MOST_COMMENTS;
+        break;
+    }
   }
 
 }
