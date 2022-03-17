2fc862ee1903ddac898d9d95df182f9a08e4384f
Wallas9_3 <drame.sissako001@gmail.com>
2022-03-17 14:51:12 +0100

    add a reply feature is added

--- src/store/effects/comment.effects.ts
+++ src/store/effects/comment.effects.ts
@@ -2,21 +2,19 @@ import { HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
-import { catchError, EMPTY, of, switchMap } from "rxjs";
+import { catchError, EMPTY, map, of, switchMap } from "rxjs";
 
 import * as fromCommentActions from "store/actions/comment.action";
 import * as fromRouterActions from "store/actions/router.actions";
 import * as fromApp from "store/reducers/index";
 import * as fromComment from "store/reducers/comment.reducers";
 import { CommentService } from "@/app/shared/services/comment.service";
-import { ReplyService } from "@/app/shared/services/reply.service";
 
 @Injectable()
 export class CommentEffects {
   suggestionId: number;
   constructor(private actions$: Actions,
     private commentService: CommentService,
-    private replyService: ReplyService,
     private store: Store<fromApp.AppState>) { }
 
   fetchComments$ = createEffect(() => this.actions$.pipe(
@@ -27,7 +25,6 @@ export class CommentEffects {
         .pipe(switchMap((comments: fromComment.AppMessage[]) => {
           const redirectTo = `feedbacks/${this.suggestionId}`
           this.store.dispatch(new fromCommentActions.FetchCommentsSucceeded(comments));
-          this.store.dispatch(new fromCommentActions.FetchRepliesStart(this.suggestionId));
           this.store.dispatch(new fromRouterActions.RedirectTo(true, redirectTo));
           return of()
         }),
@@ -39,11 +36,21 @@ export class CommentEffects {
     ofType(fromCommentActions.POST_COMMENT_START),
     switchMap(({ comment }: fromCommentActions.PostCommentStart) => {
       return this.commentService.postOneComment(comment)
-        .pipe(switchMap((comments: fromComment.AppMessage[]) => {
-          this.store.dispatch(new fromCommentActions.PostCommentSucceeded(comments))
+        .pipe(switchMap((comment: fromComment.AppMessage) => {
+          this.store.dispatch(new fromCommentActions.FetchCommentsStart(comment.suggestionId))
           return EMPTY
         }),
           catchError((error: HttpErrorResponse) => of(new fromCommentActions.PostCommentFailed(error))))
     })
   ));
+
+  postReply$ = createEffect(() => this.actions$.pipe(
+    ofType(fromCommentActions.POST_REPLY_START),
+    switchMap(({ reply }: fromCommentActions.PostReplyStart) => this.commentService.postReply(reply)),
+    switchMap((reply: fromComment.AppMessage) => {
+      this.store.dispatch(new fromCommentActions.FetchCommentsStart(reply.suggestionId));
+      return EMPTY
+    }),
+    catchError((error) => of(error)))
+  );
 }
